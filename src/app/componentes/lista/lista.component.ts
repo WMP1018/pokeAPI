5d523236f99d28cbf1512164e76ee64678f24b55
Wandy Muñoz <whandy_1018@hotmail.com>
2024-09-08 19:13:50 -0500

    PokeAPI inicial

---
+++ src/app/componentes/lista/lista.component.ts
@@ -0,0 +1,53 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { PokeapiService } from '../../servicios/pokeapi.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogoComponent } from '../dialogo/dialogo.component';
+
+@Component({
+  selector: 'app-lista',
+  standalone: true,
+  imports: [HttpClientModule, MatCardModule, MatButtonModule],
+  providers: [PokeapiService],
+  templateUrl: './lista.component.html',
+  styleUrl: './lista.component.scss'
+})
+export class ListaComponent implements OnInit{
+  listaPokemones: any;
+  pokemonesCompleto: any[] = []
+  constructor(private pokeApi: PokeapiService) {}
+  readonly dialog = inject(MatDialog);
+
+  ngOnInit(): void {
+    this.pokeApi.obtenerListadoPokemones().subscribe({
+      next: (data: any) => {
+        this.listaPokemones = data;
+        this.listaPokemones.results.forEach((element: any) => {
+          this.pokeApi.obtenerUnPokemon(element.url).subscribe({
+            next:(data:any) => {
+              this.pokemonesCompleto.push(data)
+            },
+          })          
+        });
+        console.log(this.listaPokemones);
+        console.log(this.pokemonesCompleto);
+      },
+      error: (err: any) => {console.log(err)}
+    })
+  }
+
+  playSound(soundSource: string){
+    const audio = new Audio();
+    audio.src = soundSource;
+    audio.load();
+    audio.play();
+    }
+
+  openDialog(): void {
+    this.dialog.open(DialogoComponent, {
+       width: '500px',        
+      });
+  }
+}
